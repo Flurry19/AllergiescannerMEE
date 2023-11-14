f973ddc0046149dc2c0bf279edee021a037ca332
S-witsel <1059566@hr.nl>
2023-11-14 11:10:09 +0100

    homepage frontend v1.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <div class="flex flex-col h-screen">
-    
-      <div class="bg-orange-500 border-b-2 border-black h-32">
-        <div class=" flex flex-row justify-between">
-          <p class="">AAAAAAAAAAA</p>
-          <h1 class="font-bold text-5xl text-white">MEE ROTTERDAM</h1>
-          <p class="">AAAAAAAAAAA</p>
-        </div>
-
-        <div>
-          <p class="text-center">Hier is een huisje</p>
-        </div>
-      </div>
 
       <div class="bg-white flex-auto">
         <div class="text-black font-bold text-center">
@@ -34,13 +22,7 @@ import TheWelcome from '../components/TheWelcome.vue'
           <button class="bg-orange-500 w-40 m-5 rounded-lg">Geschiedenis</button>
         </div>
       </div>
-
-      <div class="bg-orange-500 flex flex-row text-center h-32">
-        <p class="basis-1/3 border-2 border-black">AAAAAAAAAAAA</p>
-        <p class="basis-1/3 border-2 border-black">AAAAAAAAAAAA</p>
-        <p class="basis-1/3 border-2 border-black">AAAAAAAAAAAA</p>
-      </div>
-    
+      
     </div>
   </main>
 </template>
