e36fa43ba668314d47d9b3019b78e7fd6394e409
Flurry19 <sharonirenedewit@gmail.com>
2024-01-03 14:34:28 +0100

    css homepage

--- src/views/IngredientlistSearchView.vue
+++ src/views/IngredientlistSearchView.vue
@@ -1,20 +1,6 @@
 <template>
   <div>
-<!--    <header style="background-color: #EF761F; display: flex; justify-content: space-between; align-items: center; padding: 10px; height: 60px; position: fixed; width: 100%; top: 0;">-->
-<!--      <div>-->
-<!--        <img src="your-logo-image.png" alt="Site Logo">-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <h1 style="color: white; margin: 0; font-weight: bold; font-size: 2em;">MEE ROTTERDAM</h1>-->
-<!--      </div>-->
-<!--      <div>-->
-<!--        <button>-->
-<!--          <img src="instructions-icon.png" alt="Instructions Icon">-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </header>-->
-
-    <div style="padding-top: 80px;">
+    <div class="pt-80">
       <div style="padding: 10px; background: #EE9250; margin-left: 200px; margin-right: 200px">
         <div style="text-align: center; margin: 20px;">
           <div style="display: flex; align-items: center; justify-content: center;">
