71446efc5812fc563931d3472d62bd809f6bcb50
S-witsel <1059566@hr.nl>
2024-01-03 13:46:50 +0100

    update card

--- src/components/FavoriteCard.vue
+++ src/components/FavoriteCard.vue
@@ -3,6 +3,7 @@
 <template>
     <div class="group transition hover:-translate-y-1 hover:scale-110 duration-500 bg-white rounded overflow-hidden shadow-lg ring-1 ring-slate-900/5 hover:bg-orange-700 hover:ring-orange-700">
       <img :src="productImage" alt="product">
+      <p>{{ productName }} </p>
       <div class="px-6 py-4">
         <router-link :to="{ name: 'productpage', params: { barcode: barcode } }">Ga naar product</router-link>
       </div>
